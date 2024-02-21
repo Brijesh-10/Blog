d5ef6400b4f0f4f2625613e0d632432953ed3f3e
Brijesh <brijes1820h@gmail.com>
2024-02-21 16:16:20 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,11 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config();
+mongoose.connect(process.env.MONGO).then(()=>{console.log("connected stupid");}).catch(err=>{
+    console.log("fuck off",err);
+})
 const app=express()
 app.listen(3000,()=>{
     console.log('Server is running donkey');
