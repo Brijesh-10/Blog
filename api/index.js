b1d98b0a74e9333f1856c89a73b79109e79e335f
Brijesh <brijes1820h@gmail.com>
2024-02-24 20:52:21 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,13 +2,16 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(()=>{console.log("connected stupid");}).catch(err=>{
     console.log("fuck off",err);
 })
 const app=express()
+app.use(express.json());
 app.listen(3000,()=>{
     console.log('Server is running donkey');
 });
 
 app.use('/api/user',userRoutes);
+app.use('/api/auth',authRoutes);
